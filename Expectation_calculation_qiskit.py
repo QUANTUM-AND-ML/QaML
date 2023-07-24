73f203a7ec209e88e4c1d2ece9007e6d2b98250f
Yuxiang Liu <230228212@seu.edu.cn>
2023-07-24 10:26:36 +0800

    Update Expectation_calculation_qiskit.py

--- Expectation_calculation_qiskit.py
+++ Expectation_calculation_qiskit.py
@@ -6,45 +6,68 @@ from qiskit_aer import AerSimulator, noise
 from qiskit import QuantumCircuit, transpile
 from qiskit.tools.visualization import plot_histogram
 import random
+from qiskit.algorithms.optimizers import SLSQP
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.circuit import Gate, Instruction, Parameter
+from qiskit.primitives import Estimator
+from qiskit.algorithms.minimum_eigensolvers import VQE
+from qiskit.quantum_info import SparsePauliOp
 
-class Pst_calculation_qiskit:
-    def __init__(self, circuit_list , phase_list = [], number_of_qubits = 7):
-        self.circuit_list = circuit_list
-        self.phase_list = phase_list
-        self.number_of_qubits = number_of_qubits
+# The Hamiltonian of a hydrogen molecule
+H2_op = SparsePauliOp.from_list(
+    [
+        ("IIII", -0.042),
+        ("ZIII", 0.178),
+        ("IZII", 0.178),
+        ("IIZI", -0.243),
+        ("IIIZ", -0.243),
+        ("ZZII", 0.171),
+        ("IIZZ", 0.176),
+        ("ZIZI", 0.123),
+        ("IZIZ", 0.123),
+        ("ZIIZ", 0.168),
+        ("IZZI", 0.168),
+        ("YXXY", 0.045),
+        ("XYYX", 0.045),
+        ("YYXX", -0.045),
+        ("XXYY", -0.045),
+    ]
+)
 
-    def pst_calculation_qiskit(self):
-        self.circuit = QuantumCircuit(self.number_of_qubits)
-        #self.number_rz = 0
-        for gate in self.circuit_list:
-            if gate[0] == 't':
-                self.circuit.t(gate[1])
-                #self.number_rz += 1
-            elif gate[0] == 'h':
-                self.circuit.h(gate[1])
-            elif gate[0] == 'i':
-                self.circuit.i(gate[1])
-            elif gate[0] == 'cx':
-                self.circuit.cx(gate[1],gate[2])
-            else:
-                print('beyond the set of gate operations')
-        self.circuit.barrier(i for i in range(self.number_of_qubits))
-        for gate in self.circuit_list[::-1]:
-            if gate[0] == 't':
-                self.circuit.tdg(gate[1])
-                #self.number_rz += 1
-            elif gate[0] == 'h':
-                self.circuit.h(gate[1])
-            elif gate[0] == 'i':
-                self.circuit.i(gate[1])
-            elif gate[0] == 'cx':
-                self.circuit.cx(gate[1],gate[2])
-            else:
-                print('beyond the set of gate operations')
-        self.circuit.measure_all()
-        return self.circuit
+# Calculate the expectation value of the quantum circuit under noiseless conditions.
+def expectation_calculation_qiskit(circuit_list, number_of_qubits):
+    circuit = QuantumCircuit(number_of_qubits)
+    for gate in circuit_list:
+        if gate[0] == 't':
+            circuit.t(gate[1])
+            # self.number_rz += 1
+        elif gate[0] == 'h':
+            circuit.h(gate[1])
+        elif gate[0] == 'i':
+            circuit.i(gate[1])
+        elif gate[0] == 'cx':
+            circuit.cx(gate[1], gate[2])
+        else:
+            print('beyond the set of gate operations')
+    #print(circuit)
+    # Simulation of the quantum circuit
+    simulator = Aer.get_backend('statevector_simulator')
 
-def expectation_calculation_qiskit(circuit_list, number_of_qubits , noise_model):
+    valcount = np.zeros((number_of_qubits))  # We store the expectation values here
+
+    key = np.array(list(result.get_counts().keys()))  # output bit strings
+    val = np.array(list(result.get_counts().values()))  # output bit strings probabilities
+
+    # Calculation of the rescaled expectation values
+    for i in range(len(key)):
+        for j in range(len(key[i])):
+            if key[i][j] == '1':
+                valcount[j] += val[i]
+
+    return valcount[0]
+
+# Calculate the expectation value of the quantum circuit under noisy conditions
+def expectation_calculation_qiskit_with_noise(circuit_list, number_of_qubits , noise_model):
     circuit = QuantumCircuit(number_of_qubits)
     for gate in circuit_list:
         if gate[0] == 't':
@@ -68,18 +91,6 @@ def expectation_calculation_qiskit(circuit_list, number_of_qubits , noise_model)
 
     result = sim_noise.run(circ_CNNs_noise, shots=1000).result()
 
-    #counts = result.get_counts(0)
-
-    #print(counts)
-
-    #simulator = Aer.get_backend('statevector_simulator')
-
-    # Perform circuit simulation
-    #job = execute(circuit, simulator)
-
-    # Obtain simulation result
-    #result = job.result()
-
     valcount = 0 # We store the expectation values here
 
     key = np.array(list(result.get_counts().keys()))  # output bit strings
@@ -92,3 +103,46 @@ def expectation_calculation_qiskit(circuit_list, number_of_qubits , noise_model)
         if key[i][0] == '1':
             valcount += (val[i] / 1000)
     return valcount
+
+# Calculate the ground-state energy of the hydrogen molecule for different quantum circuit structures
+def ground_state_energy_calculation(circuit_list):
+    num_qubits = 4
+    circuits = circuit_list
+    number_of_parameter =  0
+    for gate in circuits:
+        if gate[0] != 'cx':
+            number_of_parameter += 1
+
+    estimator = Estimator()
+    ansatz = QuantumCircuit(num_qubits)
+
+    # Define the parameters
+    params = [Parameter(f'theta_{i}') for i in range(number_of_parameter)]
+
+    parameter_label = 0
+    for gates in circuits:
+        if gates[0] == 'rx':
+            ansatz.rx(params[parameter_label], gates[1])
+            parameter_label += 1
+        elif gates[0] == 'ry':
+            ansatz.ry(params[parameter_label], gates[1])
+            parameter_label += 1
+        elif gates[0] == 'cx':
+            ansatz.cx(gates[1], gates[2])
+        else:
+            print('beyond the set of gate operations')
+    print(ansatz)
+    #ansatz.draw(output='mpl')
+    #plt.show()
+
+    optimizer = SLSQP(maxiter=1000)
+    #optimizer = SPSA(maxiter=1000)
+
+    #ansatz.decompose().draw("mpl", style="iqx")
+    #plt.show()
+
+    vqe = VQE(estimator, ansatz, optimizer)
+
+    result = vqe.compute_minimum_eigenvalue(H2_op)
+    #print(result)
+    return result.eigenvalue.real

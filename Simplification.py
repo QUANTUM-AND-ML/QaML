40dc857d59116fe75c7ad96dc6c4e464b3cba541
Yuxiang Liu <230228212@seu.edu.cn>
2023-07-24 11:15:20 +0800

    Update Simplification.py

--- Simplification.py
+++ Simplification.py
@@ -1,24 +1,25 @@
-#import pennylane as qml
-#from pennylane import numpy as np
 import operator
 
-
-#判断量子线路中量子们相邻量子们的列表标记和连接的具体细节
+# Judge the specific details of the list marks and connections of the neighboring quanta in the Quantum circuit
 def neighbor(ansatz,order):
     '''
-    :param ansatz:输入的量子线路结构，列表格式; Example: [('rx', 0), ('rz', 1), ('cx', 0, 1)];
-    :param order: 所查找量子门所处量子线路结构的列表标记; Example:第一个量子门，所处列表标记为0
-    :return:Control_Bit:初始为[-1,-1],单比特门只考虑这个，列表的第一位是前面相邻门的序号，两比特门是控制位对应前面序号，列表第二位是后面相邻门序号，两比特类似,
-            Controlled_Bit:初始为[-1，-1]，两比特门受控位对应相邻门的序号，列表第一位是前面相邻列表的序号，列表第二位是后面相邻门序号,
-            Quantum_Gate_Marking:初始为[0，0，0，0]，共四位的列表，前两个代表控制比特前后邻居门的类型，1表示单比特门，2表示两比特门的控制位，3表示两比特门的受控位，后两个表示受控比特的前后邻居门的类型;
+    :param ansatz: Input Quantum circuit structure, list format： Example: [('rx', 0), ('rz', 1), ('cx', 0, 1)];
+    :param order: List mark of the Quantum circuit structure where the quantum gate is located; Example: the first quantum gate is marked as 0 in the list
+    :return:Control_Bit: The initial value is [-1, -1], and single bit gates only consider this. The first bit in the list is the sequence number of the adjacent gates in front, 
+    the two bit gate is the control bit corresponding to the previous sequence number, and the second bit in the list is the sequence number of the adjacent gates in the back. The two bits are similar,
+            Controlled_Bit: Initially [-1, -1], the two bit gate controlled bits correspond to the sequence numbers of adjacent gates. 
+            The first digit in the list is the sequence number of the previous adjacent list, and the second digit in the list is the sequence number of the subsequent adjacent gates,
+            Quantum_Gate_Marking: The initial value is [0, 0, 0, 0], a list of four bits. The first two represent the types of neighboring gates before and after the control bit, 
+            1 represents a single bit gate, 2 represents the control bit of a two bit gate, 
+            3 represents the controlled bit of a two bit gate, and the last two represent the types of neighboring gates before and after the controlled bit;
     '''
     Control_Bit = [-1,-1]
     Controlled_Bit = [-1,-1]
     Quantum_Gate_Marking = [0,0,0,0]
 
-    #判断Rx,Rz前后的邻居编号
+    # Determine the neighbor numbers before and after Rx and Rz
     if ansatz[order][0] == 'h' or ansatz[order][0] == 't':
-        #寻找前面邻居编号
+        #Find previous neighbor number
         if order == 0:
             Control_Bit[0] = -1
             Controlled_Bit[0] = -1
@@ -39,9 +40,9 @@ def neighbor(ansatz,order):
                         Quantum_Gate_Marking[0] = 3
                         break
                 else:
-                    print('出现未知量子门')
+                    print('Unknown quantum gate appears')
                     break
-        #寻找后面邻居编号
+        # Find the next neighbor number
         if order == len(ansatz):
             Control_Bit[1] = -1
             Controlled_Bit[1] = -1
@@ -62,12 +63,12 @@ def neighbor(ansatz,order):
                         Quantum_Gate_Marking[1] = 3
                         break
                 else:
-                    print('出现未知量子门')
+                    print('Unknown quantum gate appears')
                     break
 
-    #判断Cx前后的邻居编号
+    # Determine the neighbor numbers before and after Cx
     elif ansatz[order][0] == 'cx':
-        #寻找前面控制比特的邻居编号
+        # Find the neighbor number of the previous control bit
         if order == 0:
             Control_Bit[0] = -1
             Controlled_Bit[0] = -1
@@ -87,7 +88,7 @@ def neighbor(ansatz,order):
                         Control_Bit[0] = i
                         Quantum_Gate_Marking[0] = 3
                         break
-        #寻找后面控制比特的邻居编号
+        # Find the neighbor number for the subsequent control bit
         if order == len(ansatz):
             Control_Bit[1] = -1
             Controlled_Bit[1] = -1
@@ -107,7 +108,7 @@ def neighbor(ansatz,order):
                         Control_Bit[1] = i
                         Quantum_Gate_Marking[1] = 3
                         break
-        #寻找前面受控制比特的邻居编号
+        # Find the neighbor number of the previously controlled bit
         if order == 0:
             Control_Bit[0] = -1
             Controlled_Bit[0] = -1
@@ -127,7 +128,7 @@ def neighbor(ansatz,order):
                         Controlled_Bit[0] = i
                         Quantum_Gate_Marking[2] = 3
                         break
-        #寻找后面受控制比特的邻居编号
+        # Find the neighbor number of the next controlled bit
         if order == len(ansatz):
             Control_Bit[1] = -1
             Controlled_Bit[1] = -1
@@ -149,10 +150,10 @@ def neighbor(ansatz,order):
                         break
 
     else:
-        print('查找的是未知的量子门')
+        print('Looking for an unknown quantum gate')
     return Control_Bit,Controlled_Bit,Quantum_Gate_Marking
 
-#搜寻量子线路中固定列表标记再参数列表中的下标,CNOT门的下标是其前面旋转门参数的下标
+# Search for the subscript in the fixed list mark and parameter list in the Quantum circuit. The subscript of the CNOT gate is the subscript of the Revolving door parameter in front of it
 def Search_parameters(ansatz, order):
     new_order = 0
     for i in range(0,order,1):
@@ -161,11 +162,11 @@ def Search_parameters(ansatz, order):
     return new_order
 
 
-#删除初始的CNOT门
+# Delete initial CNOT gate
 def rule_1(ansatz):
     '''
-    :param ansatz: 输入的量子线路结构，列表格式; Example: [('rx', 0), ('rz', 1), ('cx', 0, 1)];
-    :return: 使用规则一后的量子线路结构;
+    :param ansatz: Input Quantum circuit structure, list format; Example: [('rx', 0), ('rz', 1), ('cx', 0, 1)];
+    :return: Quantum circuit structure after using rule 1;
     '''
     new_ansatz = list(ansatz)
     Count = len(new_ansatz)
@@ -180,10 +181,9 @@ def rule_1(ansatz):
                 i = i - 1
         i = i + 1
         Count = Count - 1
-    #print('使用规则1次数:',len(ansatz) - len(new_ansatz))
     return new_ansatz, label
 
-#删除初始的t门
+# Delete the initial t-gate
 def rule_2(ansatz):
     new_ansatz = list(ansatz)
     Count = len(new_ansatz)
@@ -198,15 +198,10 @@ def rule_2(ansatz):
                 i = i - 1
         i = i + 1
         Count = Count - 1
-    #print('使用规则2次数:',len(ansatz) - len(new_ansatz))
     return new_ansatz, label
 
-#删除两个重复的CNOT门
+# Delete two duplicate CNOT gates
 def rule_3(ansatz):
-    '''
-    :param ansatz: 输入的量子线路结构，列表格式; Example: [('rx', 0), ('rz', 1), ('cx', 0, 1)];
-    :return: 使用规则一后的量子线路结构;
-    '''
     new_ansatz = list(ansatz)
     Count = len(new_ansatz)
     i = 0
@@ -222,10 +217,9 @@ def rule_3(ansatz):
                 Count = Count - 1
         i = i + 1
         Count = Count - 1
-    #print('使用规则3次数:',len(ansatz) - len(new_ansatz))
     return new_ansatz, label
 
-#删除两个相同的H门
+# Delete two identical H-gates
 def rule_4(ansatz):
     new_ansatz = list(ansatz)
     Count = len(new_ansatz)
@@ -242,10 +236,9 @@ def rule_4(ansatz):
                 Count = Count - 1
         i = i + 1
         Count = Count - 1
-    #print('使用规则4次数:',len(ansatz) - len(new_ansatz))
     return new_ansatz, label
 
-#删除最后的t门
+# Delete the last t-gate
 def rule_5(ansatz):
     new_ansatz = list(ansatz)
     Count = len(new_ansatz)
@@ -260,10 +253,9 @@ def rule_5(ansatz):
                 i = i - 1
         i = i + 1
         Count = Count - 1
-    #print('使用规则5次数:',len(ansatz) - len(new_ansatz))
     return new_ansatz, label
 
-#单比特门t向右移动
+# Single-qubit gate t moves to the right
 def rule_Tshift_right(ansatz):
     new_ansatz = list(ansatz)
     label = 0  # Indicates that the rule does not use.
@@ -276,12 +268,8 @@ def rule_Tshift_right(ansatz):
                 label = 1
     return new_ansatz, label
 
-#按照规则，化简量子线路
+# Simplify quantum circuits according to rules
 def simplification(ansatz):
-    #label = 1
-    #while label > 0:
-        #new_ansatz, label = rule_Tshift_left(ansatz)
-        #print(label)
     new_ansatz = ansatz
     sum_label = 1
     while sum_label > 0:
@@ -316,12 +304,3 @@ def simplification(ansatz):
         sum_label += label
 
     return new_ansatz
-
-
-'''Example: [('ry', 0), ('rz', 1), ('cx', 0, 1)];'''
-'''
-lst= [('h', 0), ('h', 1), ('h', 2), ('t', 2), ('cx', 2, 1)]
-print(lst)
-a, b = rule_Tshift_right(lst)
-print(a,b)
-'''
